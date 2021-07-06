51f96a34e35771662d76e68d4d272d7f25fa864f
sqqqwer <85440577+sqqqwer@users.noreply.github.com>
2021-07-07 09:31:15 +1000

    Update main.py

--- main.py
+++ main.py
@@ -129,7 +129,8 @@ def otvet_na_voice(message):
 		print(error)
 		#bot.reply_to(message, error)
 
-input('скок два плюс два????')
+q = map(int, input().split())
+print(q)
 #запуск бота/turn on bot
 bot.polling(none_stop=True)
 
