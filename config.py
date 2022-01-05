35dc7ee14f7e398826e8eb1ab497c79304c2f42b
sqqqwer <85440577+sqqqwer@users.noreply.github.com>
2022-01-05 11:14:31 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os.path
 
-TOKEN = os.getenv('APIKEY')
+TOKEN = os.environ['APIKEY']
 
 current_dir = os.path.dirname(os.path.abspath(__file__)) + "/"
 voice_message_dir = current_dir + 'voicemessage/'
