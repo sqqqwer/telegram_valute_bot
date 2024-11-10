73636a7d6ea139e32b5fd20c1ad6f9fc75b3bd7d
Владислав Чернявский <cernavskijv877@gmail.com>
2024-11-10 17:08:34 +1000

    Added start keyboard menu.

---
+++ keyboards.py
@@ -0,0 +1,18 @@
+from telebot import types
+
+VALUTE_STR = 'Валюты'
+CRIPTOVALITE_STR = 'Криптавалюты'
+SUBSCRIBE = 'Подписаться на рассылку'
+ABOUT_STR = 'О боте'
+
+def keyboard_menu_start():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.row(
+        types.KeyboardButton(VALUTE_STR),
+        types.KeyboardButton(CRIPTOVALITE_STR)
+    )
+    keyboard.row(
+        types.KeyboardButton(SUBSCRIBE),
+        types.KeyboardButton(ABOUT_STR)
+    )
+    return keyboard
