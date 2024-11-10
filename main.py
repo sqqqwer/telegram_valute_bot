2ec369fa4329c40c9593a66517100061baec8e6d
Владислав Чернявский <cernavskijv877@gmail.com>
2024-11-10 17:06:37 +1000

    Change Telebot to AsyncTeleBot.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-import telebot
-
+import asyncio
+from telebot.async_telebot import AsyncTeleBot
 from settings import TELEGRAM_BOT_TOKEN
 
 
-bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+bot = AsyncTeleBot(TELEGRAM_BOT_TOKEN)
 
 
-@bot.message_handler(commands=['start', 'help'])
-def start_message(message):
-    bot.send_message(message.chat.id, 'Добро пожаловать')
+@bot.message_handler(commands=['start'])
+async def start_message(message):
+    print(message)
+    await bot.send_message(message.chat.id, 'Добро пожаловать')
 
 
-bot.polling(none_stop=True, interval=2)
+asyncio.run(bot.polling(interval=2))
