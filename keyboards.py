660f002a8a79c4a829d5bf50ae5ff99316c4d7e6
Владислав Чернявский <cernavskijv877@gmail.com>
2024-11-10 17:11:15 +1000

    Refactor code to pep8.

--- keyboards.py
+++ keyboards.py
@@ -5,6 +5,7 @@ CRIPTOVALITE_STR = 'Криптавалюты'
 SUBSCRIBE = 'Подписаться на рассылку'
 ABOUT_STR = 'О боте'
 
+
 def keyboard_menu_start():
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.row(
